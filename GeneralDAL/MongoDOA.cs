f9e2381cb88b2db3ab39df8bcdbf6336fdfbb9ef
erezye <erezye77@gmail.com>
2016-06-18 13:20:54 +0300

    add unittest

---
+++ GeneralDAL/MongoDOA.cs
@@ -0,0 +1,27 @@
+﻿using GeneralModels;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralDAL
+{
+ 
+    public class MongoDOA
+    {
+        MongoServer server;
+        public MongoDOA()
+        {
+            MongoServerSettings mss = new MongoServerSettings()
+            {
+                Server = new MongoServerAddress("127.0.0.1", 27017)
+            };
+            server = new MongoServer(mss);
+             var db = server.GetDatabase("blog");
+            var collection = db.GetCollection<Game>("Game");
+        }
+        
+    }
+}
