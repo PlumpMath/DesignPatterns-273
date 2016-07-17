5eb2957b6062e20fcc7e9797b6c5f260f5253576
erezye <erezye77@gmail.com>
2016-07-17 22:31:54 +0300

    lat

--- DesignPatternsApp/MediatorPattern/ConcreteColleague2.cs
+++ DesignPatternsApp/MediatorPattern/ConcreteColleague2.cs
@@ -28,5 +28,5 @@ namespace DesignPatternsApp.MediatorPattern
               + message);
         }
     }
-}
+
 }
--- DesignPatternsApp/ObserverPattern/ConcreteObserver.cs
+++ DesignPatternsApp/ObserverPattern/ConcreteObserver.cs
@@ -38,4 +38,4 @@ namespace DesignPatternsApp.ObserverPattern
         }
     }
 }
-}
+
